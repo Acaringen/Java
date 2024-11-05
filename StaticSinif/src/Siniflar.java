7db91372f64dbc64ee8bf8efd30c10b2ffe6ab66
alkim <alkim@DESKTOP-2R1QUB5>
2024-11-05 21:23:49 +0300

    static verilerin çağırılması

---
+++ StaticSinif/src/Siniflar.java
@@ -0,0 +1,26 @@
+ class DisSinif {
+    static class IcSinif { // Static iç sınıf
+        public void yazdir() {
+            System.out.println("Static iç sınıftayım.");
+        }
+    }
+
+    class NormalIcSinif { // Non-static iç sınıf
+        public void yazdir() {
+            System.out.println("Normal iç sınıftayım.");
+        }
+    }
+}
+
+public class Siniflar {
+    public static void main(String[] args) {
+        // Static iç sınıfa doğrudan sınıf adı üzerinden erişim
+        DisSinif.IcSinif icSinif = new DisSinif.IcSinif();
+        icSinif.yazdir(); // Çıktı: Static iç sınıftayım.
+
+        // Non-static iç sınıfa erişim için dış sınıfın bir örneğine (nesnesine) ihtiyaç var
+        DisSinif disSinif = new DisSinif();
+        DisSinif.NormalIcSinif normalIcSinif = disSinif.new NormalIcSinif();
+        normalIcSinif.yazdir(); // Çıktı: Normal iç sınıftayım.
+    }
+}
